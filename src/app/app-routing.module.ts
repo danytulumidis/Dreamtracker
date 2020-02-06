14ca602c21d2b2817cd1a388f9fc6131fbb6d3d4
Dextavision <dextavision@gmail.com>
2020-02-06 11:16:58 +0100

    start designing dreams page and few other changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // components
-import { DreamsListComponent } from './dreams/dreams-list/dreams-list.component';
+import { DreamsComponent } from './dreams/dreams.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { GoalsListComponent } from './goals/goals-list/goals-list.component';
@@ -12,7 +12,7 @@ import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
-  { path: 'mydreams', component: DreamsListComponent},
+  { path: 'mydreams', component: DreamsComponent},
   { path: 'goals', component: GoalsListComponent},
   { path: 'signin', component: LoginComponent},
   { path: 'about', component: AboutComponent},
