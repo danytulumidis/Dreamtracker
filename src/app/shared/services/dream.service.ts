ccf709a26afe33fadb224794b17dda2d44b1bc60
Dextavision <dextavision@gmail.com>
2020-02-10 15:18:22 +0100

    small changes for dreams list and behaviour of how the goals will be displayed

--- src/app/shared/services/dream.service.ts
+++ src/app/shared/services/dream.service.ts
@@ -2,9 +2,14 @@ import { Dream } from '../models/dream.model';
 
 export class DreamsService {
     dreams: Dream[] = [{
-        name: 'Work Hard',
-        description: 'test descr',
-        goals: ['goal1','goal2']
+        name: 'Web Application',
+        description: 'develop and deploy a Web Application!',
+        goals: ['Idea for Application','Develop the Application']
+    },
+    {
+        name: 'Finish Dreamtracker',
+        description: 'Finish this application!',
+        goals: ['save dreams and goals in a database','Implement OAuth2 for User sign up/in']
     }];
 
 }
