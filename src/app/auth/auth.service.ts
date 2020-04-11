3da993df6e4231913f6ea9ce4a2360432e95025b
Dextavision <dextavision@gmail.com>
2020-04-11 18:09:15 +0200

    enabled prod login functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,29 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib/types";
-import { Auth } from 'aws-amplify';
-import { AmplifyService } from 'aws-amplify-angular';
+import { Auth } from "aws-amplify";
+import { AmplifyService } from "aws-amplify-angular";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
   signedIn: boolean;
   user: any;
 
   constructor(private amplifyService: AmplifyService) {
-    // this.amplifyService.authStateChange$.subscribe(authState => {
-    //   this.user = authState.user;
-    //   this.signedIn = authState.state === 'signedIn' && this.user;            
-    // })
+    this.amplifyService.authStateChange$.subscribe(authState => {
+      this.user = authState.user;
+      this.signedIn = authState.state === "signedIn" && this.user;
+    });
     // Just for DEV
-    this.signedIn = true;
+    // this.signedIn = true;
   }
 
   federatedSignIn(provider: string) {
-    if (provider === 'facebook') {
-      Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Facebook});
+    if (provider === "facebook") {
+      Auth.federatedSignIn({
+        provider: CognitoHostedUIIdentityProvider.Facebook
+      });
     } else {
-      Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google});
+      Auth.federatedSignIn({
+        provider: CognitoHostedUIIdentityProvider.Google
+      });
     }
   }
 
